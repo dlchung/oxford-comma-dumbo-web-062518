577422f0249dc100d6e0fe4e14d2c564f3bc0e58
Daniel Chung <danetix@gmail.com>
2018-06-17 03:30:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,11 @@
 def oxford_comma(array)
-  if(array.size == 2)
-    array.join(" and ")
-  else
-    new_array = []
-    array.each_with_index do |element, index|
-      if index < array.size - 1
-        new_array.push("#{element},")
-      else
-        if array.size - index == 1
-          new_array.push("and")
-        end
+  new_array = []
+  array.each_with_index do |element, index|
+    if index < array.size - 1
+      new_array.push("#{element},")
+    else
+      if array.size - index == 1
+        new_array.push("and")
       end
     end
   end
