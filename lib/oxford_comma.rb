731b9c933ebeb13a84ea52f361292780dfe07bd0
Daniel Chung <danetix@gmail.com>
2018-06-17 03:41:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
   new_string = ""
   array.each_with_index do |element, index|
     if index == array.size - 1
-      new_string << " and "
+      new_string << "and "
       new_string << element
     elsif
-      new_string << "#{element},"
+      new_string << "#{element}, "
     end
   end
   return new_string
