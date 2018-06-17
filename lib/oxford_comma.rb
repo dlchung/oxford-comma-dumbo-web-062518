774d8745424ba982dc28864c91e68d402884cef2
Daniel Chung <danetix@gmail.com>
2018-06-17 03:47:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
   new_string = ""
   array.each_with_index do |element, index|
     if array.size > 1
-      if index == array.size - 1 ## if 2nd to last element
+      if index == array.size - 1 ## if 2nd to last element, use "and"
         new_string << "and "
         new_string << element
-      elsif array.size == 2
+      elsif array.size == 2 ## if 2 element array, no comma
         new_string << "#{element} "
       else
         new_string << "#{element}, "
